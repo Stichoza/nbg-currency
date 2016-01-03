40ac36e43ff87d3a1f4ce08a54e6766e7e076ce6
Levan Velijanashvili <stichoza@gmail.com>
2016-01-03 07:07:58 +0400

    Empty tests

---
+++ tests/CurrencyTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Stichoza\NbgCurrency\Tests;
+
+use Stichoza\NbgCurrency\NbgCurrency;
+
+class CurrencyTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        //
+    }
+
+    public function testDate()
+    {
+        $date = NbgCurrency::date();
+    }
+
+    public function testRate()
+    {
+        // TODO
+    }
+
+    public function testText()
+    {
+        // TODO
+    }
+
+    public function testChange()
+    {
+        // TODO
+    }
+
+    public function testDiff()
+    {
+        // TODO
+    }
+}
