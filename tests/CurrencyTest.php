18abb5ff1a22076589c2075f7d7c4b6f0f728567
Levan Velijanashvili <stichoza@gmail.com>
2022-12-02 04:55:50 +0400

    Some test changes

--- tests/CurrencyTest.php
+++ tests/CurrencyTest.php
@@ -3,85 +3,83 @@ namespace Stichoza\NbgCurrency\Tests;
 
 use BadMethodCallException;
 use Carbon\Carbon;
-use Stichoza\NbgCurrency\NbgCurrency;
+use PHPUnit\Framework\TestCase;
+use Stichoza\NbgCurrency;
 
-class CurrencyTest extends \PHPUnit_Framework_TestCase
+class CurrencyTest extends TestCase
 {
-    public function setUp()
+    public function testWtfMethod(): void
     {
-        //
-    }
+        $this->expectException(BadMethodCallException::class);
 
-    /**
-     * @expectedException        BadMethodCallException
-     * @expectedExceptionMessage Method [neverGonnaGiveYouUp] does not exist
-     */
-    public function testWtfMethod()
-    {
         NbgCurrency::neverGonnaGiveYouUp();
     }
 
-    public function testDate()
+    public function testDate(): void
     {
         $date = NbgCurrency::date();
-        $this->assertInstanceOf(Carbon::class, $date);
+
+        $this->assertInstanceOf(Carbon::class, $date, 'The date() method should return Carbon instance.');
     }
 
     public function testRate()
     {
         $a = NbgCurrency::rate('usd');
         $b = NbgCurrency::rateUsd();
-        $this->assertEquals($a, $b);
-        $this->assertNotEquals($a, 0);
-        $this->assertInternalType('double', $a);
+
+        $this->assertEquals($a, $b, 'Values from magic method and rate() should be the same.');
+        $this->assertNotEquals(0, $a, 'Rate should not be zero.');
     }
 
-    public function testDescription()
+    public function testName(): void
     {
-        $a = NbgCurrency::description('usd');
-        $b = NbgCurrency::descriptionUsd();
-        $this->assertEquals($a, $b);
-        $this->assertNotEquals($a, '');
-        $this->assertInternalType('string', $a);
+        $a = NbgCurrency::name('usd');
+        $b = NbgCurrency::nameUsd();
+
+        $this->assertEquals($a, $b, 'Values from magic method and name() should be the same.');
+        $this->assertNotEquals('', $a, 'Name should not be empty.');
+        $this->assertNotNull($a, 'Name should not be null.');
     }
 
-    public function testChange()
+    public function testChange(): void
     {
         $a = NbgCurrency::change('usd');
         $b = NbgCurrency::changeUsd();
-        $this->assertEquals($a, $b);
-        $this->assertTrue(in_array($a, [-1, 0, 1]));
-        $this->assertInternalType('int', $a);
+
+        $this->assertEquals($a, $b, 'Values from magic method and change() should be the same.');
+        $this->assertContains($a, [-1, 0, 1], 'Change should be either -1, 0, or 1.');
     }
 
-    public function testDiff()
+    public function testDiff(): void
     {
         $a = NbgCurrency::diff('usd');
         $b = NbgCurrency::diffUsd();
-        $this->assertEquals($a, $b);
-        $this->assertInternalType('double', $a);
+
+        $this->assertEquals($a, $b, 'Values from magic method and diff() should be the same.');
     }
 
-    public function testGet()
+    public function testGet(): void
     {
         $object      = NbgCurrency::get('usd');
         $change      = NbgCurrency::change('usd');
         $rate        = NbgCurrency::rate('usd');
         $diff        = NbgCurrency::diff('usd');
         $date        = NbgCurrency::date();
-        $description = NbgCurrency::description('usd');
+        $name        = NbgCurrency::name('usd');
+
         $this->assertEquals($object->change, $change);
         $this->assertEquals($object->rate, $rate);
         $this->assertEquals($object->diff, $diff);
-        $this->assertEquals($object->description, $description);
+        $this->assertEquals($object->name, $name);
         $this->assertEquals($object->date->toDayDateTimeString(), $date->toDayDateTimeString());
     }
 
-    public function testUnsupportedCurrency()
+    public function testUnsupportedCurrency(): void
     {
         $a = NbgCurrency::isSupported('lol');
         $b = NbgCurrency::diff('lol');
-        $this->assertFalse($a);
-        $this->assertEquals($b, 0);
+
+        $this->assertFalse($a, 'Currency "lol" should not be supported.');
+        $this->assertEquals(0, $b, 'Diff of non-existing currency should be zero.');
     }
 }
