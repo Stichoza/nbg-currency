fceb5e0afa85b86ca89483064005d42ed44bc6b3
Levan Velijanashvili <stichoza@gmail.com>
2022-12-02 13:56:46 +0400

    Add diff

--- src/Data/Currency.php
+++ src/Data/Currency.php
@@ -6,6 +6,11 @@ use Carbon\Carbon;
 
 class Currency
 {
+    /**
+     * @var int Change direction (-1: Rate decreased, 0: No change, 1: Rate increased).
+     */
+    public readonly int $change;
+
     public function __construct(
         public readonly string $code,
         public readonly float $rate,
@@ -14,6 +19,6 @@ class Currency
         public readonly Carbon $date,
         public readonly Carbon $validFromDate,
     ) {
-        //
+        $this->change = $this->diff <=> 0;
     }
 }
